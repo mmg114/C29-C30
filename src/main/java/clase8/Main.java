a6480dc67ae7936d51d5b004318070eb930215f1
Mauricio-Munoz <mauricio.m@moabits.com>
2025-03-17 20:11:50 -0500

    array

--- src/main/java/clase8/Main.java
+++ src/main/java/clase8/Main.java
@@ -1,26 +1,40 @@
 package clase8;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int contadorWhile = 0;
+        int contadorWhile = 1;
         while (contadorWhile <= 3){
             System.out.println(contadorWhile);
             contadorWhile++;
         }
-
         System.out.println("_________________________________________________________");
-
-        int contadorDoWhile = 0;
-
+        int contadorDoWhile = 1;
         do{
             System.out.println(contadorDoWhile);
             contadorDoWhile++;
-
         }while (contadorDoWhile <= 3);
+
+        System.out.println("_________________________________________________________");
+        for (int i=1; i<=3;i++){
+            System.out.println("Numero: "+i);
+        }
+        System.out.println("_________________________________________________________");
+
+        String[] estudiantes = new String[3];
+
+      for (int i = 0; i < estudiantes.length; i++) {
+          estudiantes[i] = Math.random()+" ";
+      }
+
+        Arrays.stream(estudiantes).forEach(System.out::println);
+
 
 
     }
+
 
 
 }
