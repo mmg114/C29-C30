a8be000ab991611c632dbb22898153be6f5c3299
Mauricio-Munoz <mauricio.m@moabits.com>
2025-03-17 19:59:42 -0500

    while

---
+++ src/main/java/clase8/Main.java
@@ -0,0 +1,26 @@
+package clase8;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int contadorWhile = 0;
+        while (contadorWhile <= 3){
+            System.out.println(contadorWhile);
+            contadorWhile++;
+        }
+
+        System.out.println("_________________________________________________________");
+
+        int contadorDoWhile = 0;
+
+        do{
+            System.out.println(contadorDoWhile);
+            contadorDoWhile++;
+
+        }while (contadorDoWhile <= 3);
+
+
+    }
+
+
+}
